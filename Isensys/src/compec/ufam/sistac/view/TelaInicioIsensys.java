d1f0c67a21d5ed425aed562070330868e30c8911
felipe <felipe@asus-p8z77-dlx>
2021-04-08 23:28:34 -0400

    Adicionado suporte i18n na TelaInicioIsensys

--- Isensys/src/compec/ufam/sistac/view/TelaInicioIsensys.java
+++ Isensys/src/compec/ufam/sistac/view/TelaInicioIsensys.java
@@ -4,13 +4,17 @@ import java.awt.*;
 import javax.swing.*;
 import com.phill.libs.*;
 import com.phill.libs.ui.*;
+import com.phill.libs.i18n.*;
 
 /** Classe que exibe a tela inicial do software
  *  @author Felipe André
- *  @version 2.50, 08/07/2018 */
+ *  @version 2.7, 08/04/2021 */
 public class TelaInicioIsensys extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -6673738709226295401L;
+	
+	// Carregando bundle de idiomas
+	private final PropertyBundle bundle = new PropertyBundle("i18n/titles", null);;
 
 	/** Função principal */
 	public static void main(String[] args) {
@@ -19,31 +23,28 @@ public class TelaInicioIsensys extends JFrame {
 
 	/** Inicialização da view */
 	public TelaInicioIsensys() {
-		
-		super("IsenSys v.2.5");
-		
+		super("IsenSys v.2.7");
+				
+		// Carregando atributos gráficos
 		GraphicsHelper instance = GraphicsHelper.getInstance();
 		GraphicsHelper.setFrameIcon(this,"icon/isensys-icon.png");
 		
-		Dimension d = new Dimension(475,310);
-		
-		JPanel painel = new JPaintedPanel("img/initial-screen.jpg",d);
-		setContentPane(painel);
-		setSize(d);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setResizable(false);
+		Dimension dimension = new Dimension(475,310);
+		JPanel painel = new JPaintedPanel("img/initial-screen.jpg", dimension);
 		painel.setLayout(null);
 		
 		Icon iconEnvio  = ResourceManager.getIcon("img/botao-envio.png",442,30);
 		Icon iconPrelim = ResourceManager.getIcon("img/botao-preliminar.png",442,30);
 		Icon iconFinal  = ResourceManager.getIcon("img/botao-final.png",442,30);
 		
+		// Construindo janela
 		JButton botaoEnvio = new JButton(iconEnvio);
 		botaoEnvio.setBorderPainted(false);
 		botaoEnvio.setOpaque(false);
 		botaoEnvio.setContentAreaFilled(false);
 		botaoEnvio.addActionListener((event) -> new TelaEnvio());
+		botaoEnvio.setBounds(20, 145, 442, 30);
+		painel.add(botaoEnvio);
 		
 		JButton botaoRetornoPrelim = new JButton(iconPrelim);
 		botaoRetornoPrelim.addActionListener((event) -> new TelaRetornoPreliminar());
@@ -59,35 +60,42 @@ public class TelaInicioIsensys extends JFrame {
 		botaoRetornoFinal.setOpaque(false);
 		botaoRetornoFinal.setContentAreaFilled(false);
 		botaoRetornoFinal.setBounds(20, 229, 442, 30);
-		
 		painel.add(botaoRetornoFinal);
 		
-		String rawText     = ResourceManager.getFormat("text/pt-BR.lng");
-		String welcomeText = StringUtils.toHTML(rawText);
 		Icon isensys_icon  = ResourceManager.getIcon("img/isensys-logo.png",154,130);
 		
 		JButton botaoAjuda = new JButton(isensys_icon);
-		botaoAjuda.addActionListener((event) -> ajuda());
+		botaoAjuda.addActionListener((event) -> function_help());
 		botaoAjuda.setBorderPainted(false);
 		botaoAjuda.setOpaque(false);
 		botaoAjuda.setContentAreaFilled(false);
 		botaoAjuda.setBounds(308, 12, 154, 121);
 		painel.add(botaoAjuda);
-		botaoEnvio.setBounds(20, 145, 442, 30);
-		painel.add(botaoEnvio);
 		
-		JLabel labelInfos = new JLabel(welcomeText);
+		// Carregando texto i18n
+		final String text_info = bundle.getString("inicio-panel-info");
+		
+		JLabel labelInfos = new JLabel(text_info);
 		labelInfos.setFont(instance.getFont(20));
 		labelInfos.setBounds(20, 12, 270, 121);
 		painel.add(labelInfos);
 		
+		setSize(dimension);
+		setContentPane(painel);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
 		setVisible(true);
 		
 	}
 	
 	/** Exibe um dialog com a ajuda do sistema */
-	private void ajuda() {
-		AlertDialog.info("IsenSys");
+	private void function_help() {
+		
+		// Carregando texto i18n
+		final String inicio_button_help = bundle.getString("inicio-button-help");
+		
+		AlertDialog.info(inicio_button_help);
 	}
 	
 }
