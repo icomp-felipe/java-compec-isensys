a39abaec8bb9fa2d5be16d01f35f81680c2fc65a
felipe <felipe@asus-p8z77-dlx>
2021-04-11 17:16:55 -0400

    Código da TelaEnvio organizado

--- Isensys/src/compec/ufam/sistac/view/TelaEnvio.java
+++ Isensys/src/compec/ufam/sistac/view/TelaEnvio.java
@@ -3,87 +3,79 @@ package compec.ufam.sistac.view;
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.text.DefaultFormatter;
+import javax.swing.text.*;
 
 import com.phill.libs.*;
 import com.phill.libs.ui.*;
 import com.phill.libs.i18n.*;
-import com.phill.libs.mfvapi.MandatoryFieldsLogger;
-import com.phill.libs.mfvapi.MandatoryFieldsManager;
 import com.phill.libs.files.*;
+import com.phill.libs.mfvapi.*;
 
 import compec.ufam.sistac.io.*;
 import compec.ufam.sistac.model.*;
 
+/** Implementa a tela de processamento do arquivo de solicitações de isenção.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 2.9, 11/04/2021 */
 public class TelaEnvio extends JFrame {
 
+	// Serial
 	private static final long serialVersionUID = -1766759262038217449L;
 	
 	// Carregando bundle de idiomas
-	private final static PropertyBundle bundle = new PropertyBundle("i18n/titles", null);
+	private final static PropertyBundle bundle = new PropertyBundle("i18n/tela-envio", null);
 	
+	// Declaração de atributos gráficos
+	private final JTextField textInputName, textOutputEdital, textOutputFolder;
+	private final JSpinner spinnerOutputSequencia;
+	private final JButton buttonInputReload, buttonInputClear, buttonInputSelect;
+	private final JLabel labelInputStatus, labelInputOK, labelInputError, labelInputTotal;
+	private final ImageIcon loadingIcon = new ImageIcon(ResourceManager.getResource("img/loader.gif"));
 	
-
+	// Atributos dinâmicos
+	private ParseResult resultList;
+	private File inputFile, outputDir;
+	
+	// MFV API
+	private final MandatoryFieldsManager fieldValidator;
+	private final MandatoryFieldsLogger  fieldLogger;
+	
 	public static final int INDEXES[] = new int[]{1,2,3,4,5,6,7,8,9};
-	
-	private JTextField textInputName;
-	private JTextField textOutputFolder;
-
-	private JButton buttonInputSelect;
-	
-	private JLabel labelInputStatus;
-	
-	private JLabel labelInputOK,labelInputError,labelInputTotal;
-	
-	private ParseResult listaResultados;
-	private JTextField textOutputEdital;
-	
-	private File arquivoEntrada, dirSaida;
-	
-	private JButton buttonInputReload;
-	private JButton buttonInputClear;
-
-	private ImageIcon loading = new ImageIcon(ResourceManager.getResource("img/loader.gif"));
-	private JSpinner spinnerOutputSequencia;
 	
 	public static void main(String[] args) {
 		new TelaEnvio();
 	}
 	
-	private final MandatoryFieldsManager  fieldValidator;
-	private final MandatoryFieldsLogger fieldLogger;
-
 	public TelaEnvio() {
 		//super(bundle.getString("envio-window-title"));
 		
+		// Inicializando atributos gráficos
 		GraphicsHelper instance = GraphicsHelper.getInstance();
 		GraphicsHelper.setFrameIcon(this,"icon/isensys-icon.png");
-		
-		Font  fonte = instance.getFont ();
-		Color color = instance.getColor();
 		Dimension dimension = new Dimension(500,300);
 		
 		JPanel painel = new JPaintedPanel("img/envio-screen.jpg",dimension);
+		painel.setLayout(null);
 		setContentPane(painel);
 		
-		setSize(dimension);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		painel.setLayout(null);
+		// Recuperando ícones
+		Icon clearIcon  = ResourceManager.getIcon("icon/clear.png" ,20,20);
+		Icon reloadIcon = ResourceManager.getIcon("icon/reload.png",20,20);
+		Icon searchIcon = ResourceManager.getIcon("icon/search.png",20,20);
+		Icon exitIcon   = ResourceManager.getIcon("icon/exit.png"  ,25,25);
+		Icon exportIcon = ResourceManager.getIcon("icon/save.png"  ,25,25);
 		
-		Icon searchIcon  = ResourceManager.getIcon("icon/search.png",20,20);
-		Icon reloadIcon  = ResourceManager.getIcon("icon/reload.png",20,20);
-		Icon exitIcon    = ResourceManager.getIcon("icon/exit.png",25,25);
-		Icon exportIcon  = ResourceManager.getIcon("icon/save.png",25,25);
-		Icon clearIcon  = ResourceManager.getIcon("icon/clear.png",20,20);
+		// Recuperando fontes e cores
+		Font  fonte = instance.getFont ();
+		Color color = instance.getColor();
 		
+		// Painel 'Arquivo de Entrada'
 		JPanel painelEntrada = new JPanel();
 		painelEntrada.setOpaque(false);
 		painelEntrada.setBorder(instance.getTitledBorder("Arquivo de Entrada"));
 		painelEntrada.setBounds(12, 10, 476, 105);
-		painel.add(painelEntrada);
 		painelEntrada.setLayout(null);
+		painel.add(painelEntrada);
 		
 		JLabel labelInputName = new JLabel("Nome:");
 		labelInputName.setFont(fonte);
@@ -96,11 +88,6 @@ public class TelaEnvio extends JFrame {
 		textInputName.setFont(fonte);
 		textInputName.setBounds(65, 30, 280, 25);
 		painelEntrada.add(textInputName);
-		textInputName.setColumns(10);
-		
-		buttonInputSelect = new JButton(searchIcon);
-		buttonInputSelect.setToolTipText("Busca o arquivo de entrada");
-		buttonInputSelect.addActionListener((event) -> actionInputSelect());
 		
 		buttonInputReload = new JButton(reloadIcon);
 		buttonInputReload.addActionListener((event) -> actionInputReload());
@@ -113,6 +100,10 @@ public class TelaEnvio extends JFrame {
 		buttonInputClear.setToolTipText("Busca o arquivo de entrada");
 		buttonInputClear.setBounds(395, 30, 30, 25);
 		painelEntrada.add(buttonInputClear);
+		
+		buttonInputSelect = new JButton(searchIcon);
+		buttonInputSelect.setToolTipText("Busca o arquivo de entrada");
+		buttonInputSelect.addActionListener((event) -> actionInputSelect());
 		buttonInputSelect.setBounds(435, 30, 30, 25);
 		painelEntrada.add(buttonInputSelect);
 		
@@ -122,35 +113,36 @@ public class TelaEnvio extends JFrame {
 		painelEntrada.add(labelInputStatus);
 		
 		labelInputOK = new JLabel();
-		labelInputOK.setHorizontalAlignment(SwingConstants.CENTER);
+		labelInputOK.setHorizontalAlignment(JLabel.CENTER);
 		labelInputOK.setBounds(150, 70, 80, 20);
-		painelEntrada.add(labelInputOK);
 		labelInputOK.setForeground(new Color(0x0D6B12));
 		labelInputOK.setFont(fonte);
+		painelEntrada.add(labelInputOK);
 		
 		labelInputError = new JLabel();
-		labelInputError.setHorizontalAlignment(SwingConstants.CENTER);
+		labelInputError.setHorizontalAlignment(JLabel.CENTER);
 		labelInputError.setBounds(240, 70, 100, 20);
-		painelEntrada.add(labelInputError);
 		labelInputError.setForeground(new Color(0xBC1742));
 		labelInputError.setFont(fonte);
+		painelEntrada.add(labelInputError);
 		
 		labelInputTotal = new JLabel();
-		labelInputTotal.setHorizontalAlignment(SwingConstants.CENTER);
+		labelInputTotal.setHorizontalAlignment(JLabel.CENTER);
 		labelInputTotal.setBounds(350, 70, 105, 20);
-		painelEntrada.add(labelInputTotal);
 		labelInputTotal.setForeground(color);
 		labelInputTotal.setFont(fonte);
+		painelEntrada.add(labelInputTotal);
 		
+		// Painel 'Arquivos de Saída'
 		JPanel painelSaida = new JPanel();
 		painelSaida.setOpaque(false);
 		painelSaida.setBorder(instance.getTitledBorder("Arquivos de Saída"));
 		painelSaida.setBounds(12, 115, 476, 105);
-		painel.add(painelSaida);
 		painelSaida.setLayout(null);
+		painel.add(painelSaida);
 		
 		JLabel labelOutputEdital = new JLabel("Num do Edital:");
-		labelOutputEdital.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelOutputEdital.setHorizontalAlignment(JLabel.RIGHT);
 		labelOutputEdital.setFont(fonte);
 		labelOutputEdital.setBounds(10, 30, 110, 20);
 		painelSaida.add(labelOutputEdital);
@@ -158,12 +150,11 @@ public class TelaEnvio extends JFrame {
 		textOutputEdital = new JTextFieldBounded(6);
 		textOutputEdital.setForeground(color);
 		textOutputEdital.setFont(fonte);
-		textOutputEdital.setColumns(10);
 		textOutputEdital.setBounds(125, 30, 170, 25);
 		painelSaida.add(textOutputEdital);
 		
 		JLabel labelOutputSequencia = new JLabel("Sequência:");
-		labelOutputSequencia.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelOutputSequencia.setHorizontalAlignment(JLabel.RIGHT);
 		labelOutputSequencia.setFont(fonte);
 		labelOutputSequencia.setBounds(300, 30, 85, 20);
 		painelSaida.add(labelOutputSequencia);
@@ -180,8 +171,14 @@ public class TelaEnvio extends JFrame {
 		ne_spinnerOutputSequencia.getFormat().setGroupingUsed(false);
 		
 		JFormattedTextField spinnerField  = (JFormattedTextField) ne_spinnerOutputSequencia.getComponent(0);
-		
 		spinnerOutputSequencia.setEditor(ne_spinnerOutputSequencia);
+		
+		DefaultFormatter spinnerFormatter = (DefaultFormatter) spinnerField.getFormatter();
+		spinnerFormatter.setCommitsOnValidEdit(true);
+		
+		spinnerField.setFont(fonte);
+		spinnerField.setForeground(color);
+		spinnerField.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel labelOutputFolder = new JLabel("Pasta de Saída:");
 		labelOutputFolder.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -193,7 +190,6 @@ public class TelaEnvio extends JFrame {
 		textOutputFolder.setFont(fonte);
 		textOutputFolder.setForeground(color);
 		textOutputFolder.setEditable(false);
-		textOutputFolder.setColumns(10);
 		textOutputFolder.setBounds(125, 65, 260, 25);
 		painelSaida.add(textOutputFolder);
 		
@@ -203,18 +199,18 @@ public class TelaEnvio extends JFrame {
 		buttonOutputSelect.setBounds(394, 65, 30, 25);
 		painelSaida.add(buttonOutputSelect);
 		
-		JButton buttonExit = new JButton(exitIcon);
-		buttonExit.setToolTipText("Sai do sistema");
-		buttonExit.addActionListener((event) -> dispose());
-		buttonExit.setBounds(406, 232, 35, 30);
-		painel.add(buttonExit);
-		DefaultFormatter spinnerFormatter = (DefaultFormatter) spinnerField.getFormatter();
-		
 		JButton buttonOutputClear = new JButton(clearIcon);
 		buttonOutputClear.addActionListener((event) -> actionOutputClear());
 		buttonOutputClear.setToolTipText("Busca o arquivo de entrada");
 		buttonOutputClear.setBounds(434, 65, 30, 25);
 		painelSaida.add(buttonOutputClear);
+		
+		// Fundo da janela
+		JButton buttonExit = new JButton(exitIcon);
+		buttonExit.setToolTipText("Sai do sistema");
+		buttonExit.addActionListener((event) -> dispose());
+		buttonExit.setBounds(406, 232, 35, 30);
+		painel.add(buttonExit);
 		
 		JButton buttonExport = new JButton(exportIcon);
 		buttonExport.setToolTipText("Exporta o(s) arquivo(s)");
@@ -222,24 +218,22 @@ public class TelaEnvio extends JFrame {
 		buttonExport.setBounds(453, 232, 35, 30);
 		painel.add(buttonExport);
 
-		spinnerField.setFont(fonte);
-		spinnerField.setForeground(color);
-		spinnerField.setHorizontalAlignment(SwingConstants.CENTER);
-
-		spinnerFormatter.setCommitsOnValidEdit(true);
-		
 		// Cadastrando validação de campos
 		this.fieldValidator = new MandatoryFieldsManager();
 		this.fieldLogger    = new MandatoryFieldsLogger ();
 		
-		fieldValidator.addPermanent(labelInputName   , () -> this.arquivoEntrada != null, bundle.getString("envio-mfv-input"), false);
+		fieldValidator.addPermanent(labelInputName   , () -> this.inputFile != null, bundle.getString("envio-mfv-input"), false);
 		fieldValidator.addPermanent(labelOutputEdital, () -> {
 																final String text = textOutputEdital.getText().trim();
 																return text.length() == 6 && StringUtils.isAlphanumericStringOnly(text);
 															 }, bundle.getString("envio-mfv-edital"), false);
-		fieldValidator.addPermanent(labelOutputFolder, () -> this.dirSaida != null, bundle.getString("envio-mfv-output"), false);
+		fieldValidator.addPermanent(labelOutputFolder, () -> this.outputDir != null, bundle.getString("envio-mfv-output"), false);
 		
 		// Mostrando a janela
+		setSize(dimension);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setVisible(true);
 		
 	}
@@ -249,7 +243,7 @@ public class TelaEnvio extends JFrame {
 	/** Reprocessa o arquivo de entrada */
 	private void actionInputReload() {
 		
-		if (this.arquivoEntrada != null) {
+		if (this.inputFile != null) {
 			
 			// Atualizando a view
 			setInputProcessing();
@@ -268,7 +262,7 @@ public class TelaEnvio extends JFrame {
 	private void actionInputClear() {
 		
 		// Faz algo somente se algum arquivo foi selecionado
-		if (this.arquivoEntrada != null) {
+		if (this.inputFile != null) {
 			
 			// Montando janela de diálogo
 			final String title   = bundle.getString("envio-input-clear-title");
@@ -280,8 +274,8 @@ public class TelaEnvio extends JFrame {
 			// Limpa os campos se o usuário escolheu 'OK'
 			if (choice == AlertDialog.OK_OPTION) {
 				
-				this.arquivoEntrada  = null;
-				this.listaResultados = null;
+				this.inputFile  = null;
+				this.resultList = null;
 				
 				textInputName.setText(null);
 				labelInputStatus.setVisible(false);
@@ -303,13 +297,13 @@ public class TelaEnvio extends JFrame {
 		final String title = bundle.getString("envio-input-select-title");
 		
 		// Recuperando o arquivo de entrada
-		this.arquivoEntrada  = PhillFileUtils.loadFile(title, Constants.FileFormat.SISTAC_INPUT, PhillFileUtils.OPEN_DIALOG, null);
+		this.inputFile  = PhillFileUtils.loadFile(title, Constants.FileFormat.SISTAC_INPUT, PhillFileUtils.OPEN_DIALOG, null);
 		
 		// Faz algo somente se algum arquivo foi selecionado
-		if (this.arquivoEntrada != null) {
+		if (this.inputFile != null) {
 			
 			// Atualizando a view
-			textInputName.setText(arquivoEntrada.getName());
+			textInputName.setText(inputFile.getName());
 			setInputProcessing();
 			
 			// Processando o arquivo
@@ -329,11 +323,11 @@ public class TelaEnvio extends JFrame {
 		final String title = bundle.getString("envio-output-select-title");
 		
 		// Recuperando o diretório de saída
-		this.dirSaida = PhillFileUtils.loadDir(title, PhillFileUtils.OPEN_DIALOG, null);
+		this.outputDir = PhillFileUtils.loadDir(title, PhillFileUtils.OPEN_DIALOG, null);
 		
 		// Atualizando a view
-		if (dirSaida != null)
-			textOutputFolder.setText(dirSaida.getAbsolutePath());
+		if (outputDir != null)
+			textOutputFolder.setText(outputDir.getAbsolutePath());
 		
 	}
 	
@@ -341,7 +335,7 @@ public class TelaEnvio extends JFrame {
 	private void actionOutputClear() {
 		
 		// Faz algo somente se algum diretório foi selecionado
-		if (this.dirSaida != null) {
+		if (this.outputDir != null) {
 					
 			// Montando janela de diálogo
 			final String title   = bundle.getString("envio-output-clear-title");
@@ -353,7 +347,7 @@ public class TelaEnvio extends JFrame {
 			// Limpa o campo 'Pasta de Saída' se o usuário escolheu 'OK'
 			if (choice == AlertDialog.OK_OPTION) {
 				
-				this.dirSaida = null;
+				this.outputDir = null;
 				textOutputFolder.setText(null);
 				
 			}
@@ -384,16 +378,16 @@ public class TelaEnvio extends JFrame {
 			final String sequencia = String.format("%03d", spinnerOutputSequencia.getValue());
 			
 			// Criando arquivo de saída - Sistac
-			final File saidaSistac = SistacFile.getSistacExportName(this.arquivoEntrada, edital, sequencia);
-			SistacFile.generate(listaResultados.getListaCandidatos(), saidaSistac);
+			final File saidaSistac = SistacFile.getSistacExportName(this.inputFile, edital, sequencia);
+			SistacFile.generate(resultList.getListaCandidatos(), saidaSistac);
 			
 			// Criando arquivo de saída - Excel (apenas se houveram erros no processamento)
-			if (this.listaResultados.getListaExcecoes().size() > 0) {
+			if (this.resultList.getListaExcecoes().size() > 0) {
 				
 				final String filename = String.format("errors-%s-%s.xlsx", edital, sequencia);
-				final File saidaExcel = new File(this.arquivoEntrada, filename);
+				final File saidaExcel = new File(this.inputFile, filename);
 				
-				ExcelSheetWriter.write(listaResultados.getListaExcecoes(), saidaExcel);
+				ExcelSheetWriter.write(resultList.getListaExcecoes(), saidaExcel);
 				
 			}
 			
@@ -419,7 +413,7 @@ public class TelaEnvio extends JFrame {
 	private void setInputProcessing() {
 		
 		// Atualizando a view
-		labelInputStatus.setIcon(loading);
+		labelInputStatus.setIcon(loadingIcon);
 		labelInputStatus.setText(bundle.getString("envio-input-processing"));
 		labelInputStatus.setVisible(true);
 				
@@ -437,8 +431,8 @@ public class TelaEnvio extends JFrame {
 	private void updateStatistics() {
 		
 		// Recuperando dados
-		final int sizeERR = listaResultados.getListaExcecoes  ().size();
-		final int sizeOK  = listaResultados.getListaCandidatos().size();
+		final int sizeERR = resultList.getListaExcecoes  ().size();
+		final int sizeOK  = resultList.getListaCandidatos().size();
 		final int sizeALL = (sizeOK + sizeERR);
 		
 		// Recuperando strings i18n
@@ -475,10 +469,10 @@ public class TelaEnvio extends JFrame {
 		try {
 			
 			// Seleciona o tipo de leitor de acordo com a extensão do arquivo de entrada
-			if (arquivoEntrada.getName().endsWith("xlsx"))
-				listaResultados = ExcelSheetReader.read(arquivoEntrada, INDEXES);
+			if (inputFile.getName().endsWith("xlsx"))
+				resultList = ExcelSheetReader.read(inputFile, INDEXES);
 			else
-				listaResultados = CSVSheetReader.read(arquivoEntrada, INDEXES);
+				resultList = CSVSheetReader.read(inputFile, INDEXES);
 		
 			// Só dorme um pouco pra mostrar progresso na view
 			Thread.sleep(2000);
