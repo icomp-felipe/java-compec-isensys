3f1eee1d95553d1589e8a97e21d64512b086079c
felipe <felipe@asus-p8z77-dlx>
2021-04-18 14:41:04 -0400

    Adicionado package-info no pacote 'pdf'

--- Isensys/src/compec/ufam/sistac/pdf/package-info.java
+++ Isensys/src/compec/ufam/sistac/pdf/package-info.java
@@ -1,7 +1,5 @@
-/**
- * Pacote que encapsula as classes responsáveis pela geração do
- * relatório (edital) com resultados de processamento das isenções
- * @author Felipe André
- * @version 2.50, 08/07/2018
- */
+/** Pacote que encapsula as classes responsáveis pela geração do
+ *  relatório (edital) com resultados de processamento das isenções.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 3.0, 18/04/2021 */
 package compec.ufam.sistac.pdf;
