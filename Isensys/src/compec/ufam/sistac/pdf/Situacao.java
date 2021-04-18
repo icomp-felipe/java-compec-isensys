59d06c0f4e6e7ad772e177ea594d90c99f98f027
felipe <felipe@asus-p8z77-dlx>
2021-04-18 14:40:01 -0400

    Ajustes de desempenho das classes e recursos de situações de erro

--- Isensys/src/compec/ufam/sistac/pdf/Situacao.java
+++ Isensys/src/compec/ufam/sistac/pdf/Situacao.java
@@ -1,15 +1,16 @@
 package compec.ufam.sistac.pdf;
 
-/** Classe que representa uma situação de processamento. Utilizada para a construção do relatório
- *  @author Felipe André
- *  @version 2.50, 08/07/2018 */
+/** Classe que representa uma situação de processamento. Utilizada para a construção do relatório.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 3.0, 18/04/2021 */
 public class Situacao {
 
 	private final String id;
 	private final String motivo, descricao;
 
-	/** Construtor da classe apenas inicializando os parâmetros */
-	public Situacao(String[] args) {
+	/** Construtor da classe apenas inicializando os parâmetros.
+	 *  @param args - representa uma linha de dados extraídos do arquivo .csv */
+	public Situacao(final String[] args) {
 		
 		this.id        = args[0];
 		this.motivo    = args[1];
@@ -17,19 +18,22 @@ public class Situacao {
 		
 	}
 	
-	/** Getter do motivo de processamento (ver manual do Sistac) */
-	public String getMotivo() {
-		return motivo;
-	}
-
-	/** Getter da descrição de motivo (ver manual do Sistac) */
-	public String getDescricao() {
-		return descricao;
+	/** Getter do ID de motivo (ver manual do Sistac).
+	 *  @return Código (int) do motivo de processamento. */
+	public String getID() {
+		return this.id;
 	}
 	
-	/** Getter do ID de motivo (ver manual do Sistac) */
-	public String getID() {
-		return id;
+	/** Getter do motivo de processamento (ver manual do Sistac).
+	 *  @return String contendo o motivo de processamento. */
+	public String getMotivo() {
+		return this.motivo;
 	}
 
+	/** Getter da descrição de motivo (ver manual do Sistac).
+	 *  @return String contendo a descricao do motivo de processamento. */
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
 }
