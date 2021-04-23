4d2957bc22d42afde41ed3911fe7fe27e8801f77
felipe <felipe@asus-p8z77-dlx>
2021-04-23 16:57:53 -0400

    Ajuste de índice de importação do Excel

--- Isensys/src/compec/ufam/sistac/constants/Constants.java
+++ Isensys/src/compec/ufam/sistac/constants/Constants.java
@@ -29,9 +29,9 @@ public class Constants {
                                                      };
 		
 		/** Índices dos campos aproveitados da planilha de erros. */
-		public static final int XLSX_ERROR_SHEET[] = { 1,   // Nome do solicitante
-				                                       0,   // NIS
-				                                       2    // CPF
+		public static final int XLSX_ERROR_SHEET[] = { 2,   // Nome do solicitante
+				                                       1,   // NIS
+				                                       3    // CPF
 				                                     };
 		
 		/** Cabeçalho da planilha de erros (especificação das colunas). */
