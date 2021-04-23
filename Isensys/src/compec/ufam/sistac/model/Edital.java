76f784fd2ecd360b7ab1074633d84ab1505ec2ff
felipe <felipe@asus-p8z77-dlx>
2021-04-23 02:06:35 -0400

    Novas classes comentadas

--- Isensys/src/compec/ufam/sistac/model/Edital.java
+++ Isensys/src/compec/ufam/sistac/model/Edital.java
@@ -2,16 +2,33 @@ package compec.ufam.sistac.model;
 
 import java.io.File;
 
+/** Armazena dados referentes ao edital e instituição utilizadora do Sistac.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 1.0, 23/04/2021
+ *  @since 3.0, 22/04/2021 */
 public class Edital {
 	
-	public Edital(String cnpj, String edital, String dataEdital) {
+	// Atributos de texto
+	private String cnpj, edital, dataEdital;
+	
+	/************************ Bloco de Construtores ****************************/
+	
+	/** Construtor normal, aqui não é realizada verificação alguma de integridade nos dados.
+	 *  @param cnpj - CNPJ da instituição
+	 *  @param edital - número do edital
+	 *  @param dataEdital - data de envio do arquivo Sistac (no formato DDMMYYYY) */
+	public Edital(final String cnpj, final String edital, final String dataEdital) {
+		
 		this.cnpj = cnpj;
 		this.edital = edital;
 		this.dataEdital = dataEdital;
+		
 	}
 
-	private String cnpj, edital, dataEdital;
-	
+	/** Construtor alternativo, aqui os dados são obtidos do nome do <code>arquivo</code>.
+	 *  Este construtor está ignorando possíveis erros que podem acontecer, como <code>NullPointerException</code>
+	 *  ou <code>ArrayIndexOutOfBoundsException</code>, nesses casos, o objeto pode ficar com atributos nulos.
+	 *  @param arquivo - planilha de retorno ou erros ou arquivo de compilação */
 	public Edital(final File arquivo) {
 		
 		try {
@@ -27,6 +44,22 @@ public class Edital {
 		
 	}
 	
+	/**************************** Bloco de Getters *****************************/
+	
+	/** Getter do número de edital.
+	 *  @return String contendo o número do edital. */
+	public String getEdital() {
+		return this.edital;
+	}
+
+	/** Getter da data de envio do arquivo Sistac.
+	 *  @return String contendo a data de envio do arquivo Sistac. */
+	public String getDataEdital() {
+		return this.dataEdital;
+	}
+	
+	/** Monta o nome do arquivo de erros, com os dados internos desta classe.
+	 *  @return Um arquivo com o nome no formato 'ERROS_CNPJ_EDITAL_DATA.xlsx' */
 	public File getErrorFilename() {
 		
 		String filename = String.format("ERROS_%s_%s_%s.xlsx", this.cnpj, this.edital, this.dataEdital);
@@ -34,30 +67,32 @@ public class Edital {
 		return new File(filename);
 	}
 	
-	public boolean equals(final Edital edital) {
-		return (this.cnpj.equals(edital.cnpj)) && (this.edital.equals(edital.edital)) && (this.dataEdital.equals(edital.dataEdital));
-	}
-	
-	public boolean equalsIgnoreDate(final Edital edital) {
-		return (this.cnpj.equals(edital.cnpj)) && (this.edital.equals(edital.edital));
-	}
-	
-	public void print() {
-		System.out.format("CNPJ: %s - Edital: %s - Data do Edital: %s\n", this.cnpj, this.edital, this.dataEdital);
-	}
-
+	/** Monta o nome do arquivo de compilação, com os dados internos desta classe.
+	 *  @return Um arquivo com o nome no formato 'COMPILACAO_CNPJ_EDITAL_DATA.xlsx' */
 	public File getCompilationFilename() {
+		
 		String filename = String.format("COMPILACAO_%s_%s_%s.bsf", this.cnpj, this.edital, this.dataEdital);
 		
 		return new File(filename);
 	}
-
-	public String getEdital() {
-		return this.edital;
+	
+	/** Verifica se o <code>edital</code> passado é exatamente igual a esta instância da classe.
+	 *  @param edital - objeto edital a ser comparado
+	 *  @return 'true' se o resultado da comparação de todos os campos dos objetos for 'true';<br>'false' caso algum dado seja diferente ou nulo. */
+	public boolean equals(final Edital edital) {
+		return (this.cnpj.equals(edital.cnpj)) && (this.edital.equals(edital.edital)) && (this.dataEdital.equals(edital.dataEdital));
 	}
-
-	public String getDataEdital() {
-		return this.dataEdital;
+	
+	/** Verifica se o CNPJ e número de edital do objeto <code>edital</code> passado é exatamente igual a esta instância da classe.
+	 *  @param edital - objeto edital a ser comparado
+	 *  @return 'true' se o resultado da comparação de CNPJ e edital dos objetos for 'true';<br>'false' caso algum dado seja diferente ou nulo. */
+	public boolean equalsIgnoreDate(final Edital edital) {
+		return (this.cnpj.equals(edital.cnpj)) && (this.edital.equals(edital.edital));
+	}
+	
+	/** Imprime todos os atributos dessa classe, útil para debug. */
+	public void print() {
+		System.out.format("CNPJ: %s - Edital: %s - Data do Edital: %s\n", this.cnpj, this.edital, this.dataEdital);
 	}
 
 }
