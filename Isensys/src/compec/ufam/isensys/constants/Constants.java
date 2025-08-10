13058906ab1e81b960121827100bfd1b41587786
Felipe <Felipe@192.168.0.5>
2025-08-10 15:54:49 -0400

    Classe de constantes limpa e revisada

--- Isensys/src/compec/ufam/isensys/constants/Constants.java
+++ Isensys/src/compec/ufam/isensys/constants/Constants.java
@@ -7,6 +7,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.phill.libs.ResourceManager;
 
+@Reviewed("2025-08-10")
 /** Armazena as diversas constantes utilizadas em todo o sistema.
  *  @author Felipe André - felipeandre.eng@gmail.com
  *  @version 4.0, 10/AGO/2025 */
@@ -17,7 +18,7 @@ public class Constants {
 	
 	/** Formatos de arquivo utilizados nos métodos de seleção de arquivos.
 	 *  @author Felipe André - felipeandre.eng@gmail.com
-     *  @version 3.5, 14/11/2023 */
+     *  @version 4.0, 10/AGO/2025 */
 	public static class FileFormat {
 		
 		public static final FileNameExtensionFilter CSV = new FileNameExtensionFilter("Texto Separado por Vírgula (.csv)", "csv");
@@ -35,23 +36,8 @@ public class Constants {
 	
 	/** Índices de coluna de planilha.
 	 *  @author Felipe André - felipeandre.eng@gmail.com
-     *  @version 3.5, 23/04/2021 */
+     *  @version 4.0, 10/AGO/2025 */
 	public static class SheetIndex {
-		
-		/** Índices dos campos aproveitados do arquivo de retorno do Sistac. */
-		public static final int[] CSV_RETURN_SHEET = {
-				                                       1,   // Nome do solicitante
-                                                       2,   // CPF
-                                                       3,   // Data de nascimento
-                                                       4,  // Situação ('N' para indeferido ou 'S' para deferido
-                                                       5   // Código de situação (motivo), caso esta seja 'N' (ver manual do Sistac)
-                                                     };
-		
-		/** Índices dos campos aproveitados da planilha de erros. */
-		public static final int XLSX_ERROR_SHEET[] = { 2,   // Nome do solicitante
-				                                       1,   // NIS
-				                                       3    // CPF
-				                                     };
 		
 		/** Cabeçalho da planilha de erros (especificação das colunas). */
 		public static final String[] XLSX_COLUMN_TITLES = {"Linha", "Nome", "CPF", "Data de Nascimento", "Situação", "Motivo"};
@@ -60,7 +46,7 @@ public class Constants {
 	
 	/** Formatos de data mais utilizados no sistema.
 	 *  @author Felipe André - felipeandre.eng@gmail.com
-     *  @version 4.0, 03/AGO/2025 */
+     *  @version 4.0, 10/AGO/2025 */
 	public static class DateFormatters {
 		
 		public static final DateTimeFormatter EXCEL_DATE = DateTimeFormatter.ofPattern("dMMuuuu");
@@ -72,7 +58,7 @@ public class Constants {
 	
 	/** Formatos de String.
 	 *  @author Felipe André - felipeandre.eng@gmail.com
-     *  @version 4.0, 02/AGO/2025 */
+     *  @version 4.0, 10/AGO/2025 */
 	public static class StringFormat {
 		
 		/** Máscara de String de acordo com o formato Sistac. */
