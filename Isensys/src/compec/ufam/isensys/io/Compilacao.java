37762b6cfc1af34a37ef717e4fe420641baa8787
Felipe <Felipe@192.168.0.5>
2025-08-10 16:00:12 -0400

    Classe 'Compilacao' revisada

--- Isensys/src/compec/ufam/isensys/io/Compilacao.java
+++ Isensys/src/compec/ufam/isensys/io/Compilacao.java
@@ -1,12 +1,20 @@
 package compec.ufam.isensys.io;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import compec.ufam.isensys.model.retorno.*;
+import compec.ufam.isensys.constants.Reviewed;
+import compec.ufam.isensys.model.retorno.ListaRetornos;
+import compec.ufam.isensys.model.retorno.Retorno;
 
+@Reviewed("2025-08-10")
 /** Classe de I/O da compilação do sistema.
  *  @author Felipe André - felipeandre.eng@gmail.com
- *  @version 3.9, 05/MAR/2024 */
+ *  @version 4.0, 10/AGO/2025 */
 public class Compilacao {
 	
 	/** Salva a lista de retornos em um arquivo binário.
@@ -15,33 +23,26 @@ public class Compilacao {
 	 *  @throws IOException caso haja alguma falha na escrita do arquivo. */
 	public static void save(final ListaRetornos listaRetornos, final File arquivo) throws IOException {
 
-		// Abrindo arquivo para escrita
-		ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(arquivo));
-		
-		// Escrevendo dados
-		stream.writeObject(listaRetornos);
-		
-		// Liberando recursos
-		stream.close();
+		try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+			
+			stream.writeObject(listaRetornos);
+			
+		}
 		
 	}
 	
-	/** Recupera a compilação do 'arquivo' para o objeto ListaRetornos.
+	/** Recupera a compilação do <code>arquivo</code> para o objeto {@link ListaRetornos}.
 	 *  @param arquivo - caminho do arquivo .icf
 	 *  @throws ClassNotFoundException caso as classes {@link ListaRetornos} ou {@link Retorno} não estejam disponíveis, ou mudaram de versão;
 	 *  @throws IOException caso haja alguma falha na leitura do arquivo. */
 	public static ListaRetornos load(final File arquivo) throws ClassNotFoundException, IOException {
 		
-		// Abrindo arquivo para leitura
-		ObjectInputStream stream = new ObjectInputStream(new FileInputStream(arquivo));
+		try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(arquivo))) {
+			
+			return (ListaRetornos) stream.readObject();
+			
+		}
 		
-		// Recuperando dados
-		ListaRetornos listaRetornos = (ListaRetornos) stream.readObject();
-		
-		// Liberando recursos
-		stream.close();
-		
-		return listaRetornos;
 	}
 	
 }
